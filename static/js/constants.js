ff228e144e7066d7569580488f101bbed286ef05
Liafau <58419564+Liafau@users.noreply.github.com>
2021-07-28 20:53:41 +0700

    change url

--- static/js/constants.js
+++ static/js/constants.js
@@ -1,3 +1,3 @@
 const action_name = "action_hello_world";
-const rasa_server_url = "https://chatbotspmbpoltek.herokuapp.com/webhooks/rest/webhook";
+const rasa_server_url = "https://chatbotpoltekssn.herokuapp.com/webhooks/rest/webhook";
 const sender_id = uuidv4();
