2b2bbfc481a01afbf54893d3dc6e854598d64779
Liafau <58419564+Liafau@users.noreply.github.com>
2021-07-29 10:41:58 +0700

    Update constants.js

--- static/js/constants.js
+++ static/js/constants.js
@@ -1,3 +1,3 @@
 const action_name = "action_hello_world";
-const rasa_server_url = "https://chatbotspmbpoltek.herokuapp.com/webhooks/rest/webhook";
+const rasa_server_url = "https://bismillahspmb.herokuapp.com/webhooks/rest/webhook";
 const sender_id = uuidv4();
